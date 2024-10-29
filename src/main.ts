760187843f618d1329172de117be23acf70d6229
lizirui <1037460200@qq.com>
2024-10-29 13:48:48 +0800

    test: 测试仓库

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: ZiRui
+ * @LastEditors: ZiRui
+ * @Date: 2024-10-29 11:33:16
+ * @LastEditTime: 2024-10-29 13:48:23
+ * @Description: 
+ */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,4 +12,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+console.log('test')
 bootstrap();
